8c9c083143099d27a406785225bcd304d70c9caa
Alexia Friedman <alexia@cognosis.com>
2018-01-03 21:40:34 -0800

    Changed title bar to toolbar

--- app/src/main/java/com/example/alexi/travelapp/MainActivity.java
+++ app/src/main/java/com/example/alexi/travelapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.alexi.travelapp;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.Window;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (findViewById(R.id.my_toolbar));
+        setSupportActionBar(myToolbar);
     }
 }
