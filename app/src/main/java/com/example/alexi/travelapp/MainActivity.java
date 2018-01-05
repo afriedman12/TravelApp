6293fbe3434be8b408aa3ec7f28c945bc227bc46
Alexia Friedman <alexia@cognosis.com>
2018-01-04 21:59:47 -0800

    Added hamburger icon

--- app/src/main/java/com/example/alexi/travelapp/MainActivity.java
+++ app/src/main/java/com/example/alexi/travelapp/MainActivity.java
@@ -3,8 +3,8 @@ package com.example.alexi.travelapp;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
-import android.view.Window;
-
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (findViewById(R.id.my_toolbar));
         setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_show_menu:
+                //display or hide menu
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
