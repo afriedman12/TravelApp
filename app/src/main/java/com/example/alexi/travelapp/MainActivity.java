b32a1d3aabdd34d5d3db4d2efb12c5cb540fb8af
Alexia Friedman <alexia@cognosis.com>
2018-01-07 20:48:58 -0800

    Added search bar

--- app/src/main/java/com/example/alexi/travelapp/MainActivity.java
+++ app/src/main/java/com/example/alexi/travelapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.alexi.travelapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         switch(item.getItemId()){
             case R.id.action_show_menu:
-                //display or hide menu
-                return true;
+                LinearLayout tripList = findViewById(R.id.tripLayout);
+                tripList.setVisibility(tripList.isShown() ? View.GONE:View.VISIBLE);
 
             default:
                 return super.onOptionsItemSelected(item);
